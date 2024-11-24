818263c7619fb909431074f50ae2dbf32c1ca7ed
Chandu Kalisetti <chandu.s1729@gmail.com>
2024-11-24 18:08:01 +0000

    first commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,35 @@
+import Header from '../Header'
+import './index.css'
+
+const Home = () => (
+  <>
+    <Header />
+    <div className="home-container">
+      <div className="home-content">
+        <h1 className="home-heading">FormaAI it Shapes your Lifestyle</h1>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-img.png"
+          alt="dresses to be noticed"
+          className="home-mobile-img"
+        />
+        <p className="home-description">
+          Forma AI helps you create a personalized daily schedule based on your
+          activities, food preferences, and health goals. Simply enter your
+          details, and the app generates a plan tailored to your needs,
+          including tasks, meals, and reminders. Stay organized and balanced
+          effortlessly with AI-guided recommendations!
+        </p>
+        <button type="button" className="shop-now-button">
+          Fill the Details
+        </button>
+      </div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-img.png"
+        alt="dresses to be noticed"
+        className="home-desktop-img"
+      />
+    </div>
+  </>
+)
+
+export default Home
